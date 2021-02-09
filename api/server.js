c75c8ac8cee8a9de131e838909a8d436437dca65
Kiwouz <espritkiwi@gmail.com>
2021-02-09 11:51:25 +0100

    CRUD #5

--- api/server.js
+++ api/server.js
@@ -22,49 +22,85 @@ app.use(bodyParser.urlencoded({extended : true}));
 
 app.use(busboy());
 
+// Connection base de donnée
+let mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+let database = mongoose.connect(
+    "mongodb://localhost/demo",
+    {
+        promiseLibrary: require('bluebird'),
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+// Charger le model
+const SaladModel = require('./Salad');
+const Salad = mongoose.model('Salad');
+
 // ------------------------
 // LIST ROUTE ou EndPoint
 // ------------------------
 // -- Listes des salades
 app.get('/salads', function (req, res) {
-    res.status(400).json({message : "todo"})
+    Salad.find({}).then((salads)=>{
+        res.status(200).json(salads)
+    },(err)=>{
+        res.status(400).send(err)
+    })
 });
 
 // -- Consulter une salade
-app.get('/salads/:id', function (req, res) {
-    res.status(400).json({message : "todo"})
+app.get('/salads/:idSalad', function (req, res) {
+    Salad.findOne({id : req.params.idSalad}).then((salad)=>{
+        if(salad){
+            res.status(200).json(salad)
+        }else{
+            res.status(404).json({message : "Salad not found - "+req.params.idSalad})
+        }
+    },(err)=>{
+        res.status(400).send(err)
+    })
 });
 // -- Upload Salade
 app.post('/salads', function (req, res) {
     // ---
     console.info("Upload a file");
-    res.status(400).json({message : "todo to upload"})
+
+    // -- Create Classique
+    let mySalad = new Salad(req.body);
+    mySalad.id = mySalad._id;
+
+    mySalad.save().then((salad)=>{
+        res.status(200).json(salad)
+    },(err)=>{
+        res.status(400).send(err)
+    })
 
     // --- Catch dans busboy le stream de mes images
-    req.pipe(req.busboy);
-
-    try{
-        req.busboy.on('file', function (fieldname, file, filename) {
-
-            console.info("Uploading: " + filename);
-
-            // --- Create a path to the image
-            let fstream = fs.createWriteStream(path.join(__dirname, filename.replace(' ', '-')));
-
-            file.pipe(fstream);
-
-            fstream.on('close', function (error) {
-                if(error){
-                    res.status(400).json(error);
-                }else{
-                    // --- Update the object to get the link
-                    res.status(204).json({});
-                }
-            });
-        });
-    }catch(error){
-        res.status(400).json(error);
-    }
+    // req.pipe(req.busboy);
+    //
+    // try{
+    //     req.busboy.on('file', function (fieldname, file, filename) {
+    //
+    //         console.info("Uploading: " + filename);
+    //
+    //         // --- Create a path to the image
+    //         let fstream = fs.createWriteStream(path.join(__dirname, filename.replace(' ', '-')));
+    //
+    //         file.pipe(fstream);
+    //
+    //         fstream.on('close', function (error) {
+    //             if(error){
+    //                 res.status(400).json(error);
+    //             }else{
+    //                 // --- Update the object to get the link
+    //                 res.status(204).json({});
+    //             }
+    //         });
+    //     });
+    // }catch(error){
+    //     res.status(400).json(error);
+    // }
 });
 
 // ------------------------
